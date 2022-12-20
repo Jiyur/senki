2a17f46747ae514544032ead5901d8494db45fa8
Quang Khanh <53509652+Jiyur@users.noreply.github.com>
2022-12-20 10:17:05 +0700

    implement reset password

--- src/main/java/com/abc/senki/service/implement/EmailServiceImpl.java
+++ src/main/java/com/abc/senki/service/implement/EmailServiceImpl.java
@@ -47,7 +47,7 @@ public class EmailServiceImpl implements EmailService {
             Template template= config.getTemplate("email-temp.ftl");
             Map<String, Object> model=new HashMap<>();
             model.put("fullName",user.getFullName());
-            model.put("link",host+"/token="+jwtUtils.generateEmailJwtToken(user.getEmail()));
+            model.put("link",host+"/reset?token="+jwtUtils.generateEmailJwtToken(user.getEmail()));
             String html = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
             helper = new MimeMessageHelper(message, true);
             helper.setFrom("Senki");
