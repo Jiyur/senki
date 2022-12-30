86df2e1fa33316c6fb416cc4780da6ca5fcbd38f
Quang Khanh <53509652+Jiyur@users.noreply.github.com>
2022-12-30 12:25:13 +0700

    fix user entity constraint

--- src/main/java/com/abc/senki/model/entity/UserEntity.java
+++ src/main/java/com/abc/senki/model/entity/UserEntity.java
@@ -30,7 +30,7 @@ public class UserEntity {
     private UUID id;
 
     @JsonIgnore
-    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "\"user_role\"", joinColumns = @JoinColumn(name = "\"user_id\""), inverseJoinColumns = @JoinColumn(name = "\"role_id\""))
     private Set<RoleEntity> roles;
 
