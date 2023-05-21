588ea6ab04c543850e04d3c0978f7b4da267c9f2
Quang Khanh <53509652+Jiyur@users.noreply.github.com>
2023-05-21 16:37:16 +0700

    [fix] get seller list api logic

--- src/main/java/com/abc/senki/controller/client/ChatController.java
+++ src/main/java/com/abc/senki/controller/client/ChatController.java
@@ -1,13 +1,17 @@
 package com.abc.senki.controller.client;
 
 import com.abc.senki.model.entity.UserEntity;
+import com.abc.senki.model.payload.response.SuccessResponse;
 import com.abc.senki.service.UserService;
+import com.abc.senki.util.DataUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -16,9 +20,10 @@ public class ChatController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("seller/list")
+    @GetMapping("seller/list")
     public ResponseEntity<Object> getSeller(@RequestParam("nickname") String nickname) {
         List<UserEntity> sellerList=userService.getSellerByNickname(nickname);
-        return ResponseEntity.ok(sellerList);
+        return ResponseEntity.ok(new SuccessResponse("Get list seller success",
+                DataUtil.getData("sellerList",sellerList)));
     }
 }
