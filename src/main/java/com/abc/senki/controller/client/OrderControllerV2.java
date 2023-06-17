59167b03f63207fbb1ba70093580f5fd6d281b73
KHANH <vcatanh@gmail.com>
2023-06-17 19:01:44 +0700

    [fix] paypal service #5

--- src/main/java/com/abc/senki/controller/client/OrderControllerV2.java
+++ src/main/java/com/abc/senki/controller/client/OrderControllerV2.java
@@ -101,9 +101,9 @@ public class OrderControllerV2 {
     @GetMapping("/pay/success/{id}")
     @Operation(summary = "Paypal payment success")
     public ResponseEntity<Object> successPay(@PathVariable("id") String id,
-                                             @RequestParam("paymentId") String paymentId,
-                                             @RequestParam("redirectURI") String uri,
-                                             @RequestParam("PayerID") String payerId,
+                                             @RequestParam(name = "paymentId") String paymentId,
+                                             @RequestParam(name = "redirectURI") String uri,
+                                             @RequestParam(name = "PayerID") String payerId,
                                              HttpServletResponse response
                                             ){
         //Execute payment
