06055c9338fb632d1ed211bf5a493c47426e29c8
KHANH <vcatanh@gmail.com>
2023-06-21 22:07:00 +0700

    [fix] userEntity get Id

--- src/main/java/com/abc/senki/controller/global/SellerLiscenseController.java
+++ src/main/java/com/abc/senki/controller/global/SellerLiscenseController.java
@@ -108,7 +108,7 @@ public class SellerLiscenseController {
             }
             userService.saveInfo(userEntity);
 
-            response.sendRedirect(REDIRECT_HOST);
+            response.sendRedirect(REDIRECT_HOST+"?"+userEntity.getId());
         }
         return ResponseEntity.badRequest().body(new ErrorResponse("Payment error", HttpStatus.BAD_REQUEST.value()));
 
